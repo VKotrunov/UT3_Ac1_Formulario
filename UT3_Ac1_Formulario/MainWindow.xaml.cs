775bfd4867cd25ff1e21cd05e4001cc6e6c1c33c
VKotrunov <57618856+VKotrunov@users.noreply.github.com>
2020-10-27 19:21:59 +0100

    Agregar archivos de proyecto.

---
+++ UT3_Ac1_Formulario/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows;
+
+namespace UT3_Ac1_Formulario
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
